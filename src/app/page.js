ead349251d340d80b19d8f6f2e5bac1c5bd4571e
gyaner <ganeshkanyal192@gmail.com>
2025-08-18 18:46:55 +0530

    changes in pages

--- src/app/page.js
+++ src/app/page.js
@@ -71,7 +71,7 @@ export default function Home() {
             src="/window.svg"
             alt="Window icon"
             width={16}
-            height={16}
+            height={18}
           />
           Examples
         </a>
