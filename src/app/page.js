b32b14dda3b31c98f39a14b4097015b2cb37729c
gyaner <153911085+gyaner@users.noreply.github.com>
2025-08-19 12:54:42 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={48}
           priority
         />
         <ol>
